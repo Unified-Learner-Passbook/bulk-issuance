8808d00ef2932242d8204a4b81b3f506940f2d79
Rushi Gawali <rushi_g@tekditechnologies.com>
2023-09-12 01:44:39 +0530

    code refactor, api endpoint organize

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const port = process.env.PORT;
+const port = process.env.PORT || 3007;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
