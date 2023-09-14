b566bf948c39a03f317bc429c07a52d5ca303f38
Rushi Gawali <rushi_g@tekditechnologies.com>
2023-09-14 20:21:29 +0530

    credentials issue fix from 3.0.0 to 1.0.0

--- src/services/credentials/credentials.service.ts
+++ src/services/credentials/credentials.service.ts
@@ -173,9 +173,8 @@ export class CredentialsService {
         },
       },
       credentialSchemaId: payload.credSchema.id,
-      credentialSchemaVersion: '3.0.0',
+      credentialSchemaVersion: '1.0.0',
       tags: ['tag1', 'tag2', 'tag3'],
-      method: 'cbse',
     });
 
     const url = `${process.env.CRED_URL}/credentials/issue`;
